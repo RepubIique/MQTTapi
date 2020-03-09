10c0c0e7514d27dff015a26395f95079d039fe83
Ken <kenpoon94@gmail.com>
2020-03-09 21:08:25 +0800

    Fix cors issue

--- server/server.js
+++ server/server.js
@@ -1,13 +1,15 @@
 const express = require('express');
 const apirouter = require('./routes');
+const cors = require('cors');
 
 const app = express();
 
+app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended : false}))
 
 app.use('/api/',apirouter);
 
-app.listen(process.env.PORT || '3000', () => {
-    console.log(`Server is runing on port: ${process.env.PORT || '3000'}`);
+app.listen(process.env.PORT || '8080', () => {
+    console.log(`Server is runing on port: ${process.env.PORT || '8080'}`);
 });
