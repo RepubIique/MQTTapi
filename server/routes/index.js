9db6648aae6e3c4456b8474b884fef670d05d4b7
Ken <kenpoon94@gmail.com>
2020-03-11 23:46:15 +0800

    Adding stripe api endpoint

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const db = require('../db');
 const router = express.Router();
+const stripe = require("stripe")("sk_test_cupKvj0K2Ty0I3ZVar0Vk5PN00ioJfhbWc");
 
 
 // Testing
@@ -51,6 +52,58 @@ router.get('/listproducts', async(req, res)=>{
 })
 
 
+router.post('/pay', async (request, response) => {
+    try {
+      let intent;
+      if (request.body.payment_method_id) {
+        // Create the PaymentIntent
+        intent = await stripe.paymentIntents.create({
+          payment_method: request.body.payment_method_id,
+          amount: 1099,
+          currency: 'usd',
+          confirmation_method: 'manual',
+          confirm: true
+        });
+      } else if (request.body.payment_intent_id) {
+        intent = await stripe.paymentIntents.confirm(
+          request.body.payment_intent_id
+        );
+      }
+      // Send the response to the client
+      response.send(generateResponse(intent));
+    } catch (e) {
+      // Display error on client
+      return response.send({ error: e.message });
+    }
+  })
+
+
+  const generateResponse = (intent) => {
+    // Note that if your API version is before 2019-02-11, 'requires_action'
+    // appears as 'requires_source_action'.
+    if (
+      intent.status === 'requires_action' &&
+      intent.next_action.type === 'use_stripe_sdk'
+    ) {
+      // Tell the client to handle the action
+      return {
+        requires_action: true,
+        payment_intent_client_secret: intent.client_secret
+      };
+    } else if (intent.status === 'succeeded') {
+      // The payment didn’t need any additional actions and completed!
+      // Handle post-payment fulfillment
+      return {
+        success: true
+      };
+    } else {
+      // Invalid status
+      return {
+        error: 'Invalid PaymentIntent status'
+      }
+    }
+  };
+  
 
 const isEmpty = (obj) =>{
     for(var key in obj) {
