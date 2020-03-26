a95526bd6f5a4a3b09eab1422f99f0ce952624cc
Kendrick Bong <Kendrickbong1996@gmail.com>
2020-03-26 20:44:18 +0800

    h

--- server/server.js
+++ server/server.js
@@ -1,15 +1,15 @@
-const express = require('express');
-const apirouter = require('./routes');
-const cors = require('cors');
+const express = require("express");
+const apirouter = require("./routes");
+const cors = require("cors");
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
-app.use(express.urlencoded({ extended : false}))
+app.use(express.urlencoded({ extended: false }));
 
-app.use('/api/',apirouter);
+app.use("/api/", apirouter);
 
-app.listen(process.env.PORT || '8080', () => {
-    console.log(`Server is runing on port: ${process.env.PORT || '8080'}`);
+app.listen(process.env.PORT || "8080", () => {
+  console.log(`Server is runing on port: ${process.env.PORT || "8080"}`);
 });
